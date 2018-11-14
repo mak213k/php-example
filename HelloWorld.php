3b0a2a43af7304048590e3a8e03a886e66365afa
Mayko Rodrigues <mak213k@yahoo.com.br>
2018-11-14 17:36:52 +0000

    Update HelloWorld.php
    echo "teste 14/11/2018 as 15:36";

--- HelloWorld.php
+++ HelloWorld.php
@@ -10,14 +10,16 @@ class HelloWorld
     public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
-        echo "teste 14/11/2018 as 15:24";
+        echo "teste 14/11/2018 as 15:24 0";
     }
 
     public function hello($what = 'World')
     {
         $sql = "INSERT INTO hello VALUES (" . $this->pdo->quote($what) . ")";
         $this->pdo->query($sql);
+        echo "teste 14/11/2018 as 15:24 1";
         return "Hello $what";
+        
     }
 
 
@@ -26,6 +28,7 @@ class HelloWorld
         $sql = "SELECT what FROM hello";
         $stmt = $this->pdo->query($sql);
         $result = $stmt->fetchColumn();
+        echo "teste 14/11/2018 as 15:24 2";
         return $result;
     }
 }
